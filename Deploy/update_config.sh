43c0834936fa3c887fd0f054ee49cd092c8eb4aa
blacree <emmanuelogunfowokan@gmail.com>
2022-11-27 19:03:24 +0100

    specifc config and mount test 2.5

--- Deploy/update_config.sh
+++ Deploy/update_config.sh
@@ -10,7 +10,7 @@ sed -i 's@"m1: 127.0.0.1"@'""m1:$private_host_ip","m2:$m2","m3:$m3","m4:$m4""'@'
 # sed -i 's/"m1: 127.0.0.1"/"m1:$private_host_ip","m2:$m2","m3:$m3","m4:$m4"/g' install_conf.json
 sed -i 's@home@'"data"'@' install_conf.json
 sed -i 's@sudoUserName@'"ubuntu"'@' install_conf.json
-sed -i 's@/path/to/my_key.pem_rsa@'"/data/tigergraph/tigergraph.pem"'@' install_conf.json
+sed -i 's@/path/to/my_key.pem_rsa@'"/data/tigergraph/blacree"'@' install_conf.json
 # sed -i '/comment/d' install_conf.json
 sed -i 's/P\[or K\]/K/g' install_conf.json
 sed -i 's/ReplicationFactor": 1/ReplicationFactor": 2/g' install_conf.json
