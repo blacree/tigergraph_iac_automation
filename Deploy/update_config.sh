fe50717dd82507d562d1ff5726f27e53795a7192
blacree <emmanuelogunfowokan@gmail.com>
2022-11-27 16:45:45 +0100

    specifc config and mount test 2.1

--- Deploy/update_config.sh
+++ Deploy/update_config.sh
@@ -1,7 +1,7 @@
-# private_host_ip=`echo $(hostname -I)`
+private_host_ip=`echo $(hostname -I)`
 
 
-# sed -i 's@127.0.0.1@'"$private_host_ip"'@' install_conf.json
+sed -i 's@127.0.0.1@'"$private_host_ip"'@' install_conf.json
 sed -i 's@home@'"data"'@' install_conf.json
 sed -i 's@sudoUserName@'"ubuntu"'@' install_conf.json
 sed -i 's@/path/to/my_key.pem_rsa@'"/data/tigergraph/tigergraph.pem"'@' install_conf.json
